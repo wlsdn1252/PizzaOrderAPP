1e347c935cdefb68e103af9ec23cb1b3899db93e
강진우 <wlsdn1252@naver.com>
2023-01-15 18:31:10 +0900

    치킨가게 목록용 프래그먼트 작성

---
+++ app/src/main/java/com/example/pizzaorderapp/fragments/ChickenStoreListFragment.kt
@@ -0,0 +1,25 @@
+package com.example.pizzaorderapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizzaorderapp.R
+
+class ChickenStoreListFragment : Fragment() {
+
+    // fragment_pizza_store_list.xml이랑 연결 시키기
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chicken_store_list, container,false)
+    }
+
+    // 동작에 관련된 코드
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
