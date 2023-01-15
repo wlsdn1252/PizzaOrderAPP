c8f41986817acd5c0381791404f53cf68900e983
강진우 <wlsdn1252@naver.com>
2023-01-15 18:14:31 +0900

    메인액티비티가 BaseActivity를 상속 BaseActivity를 사용하기위한 오버라이딩

--- app/src/main/java/com/example/pizzaorderapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp/MainActivity.kt
@@ -3,9 +3,27 @@ package com.example.pizzaorderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
     }
+
+    // BaseActivity에서 선언한 함수들을 재사용하기위해 오버라이딩
+    // 버튼 크릭등의 이벤트 관련 코드 모음
+    override fun setupEvents() {
+
+    }
+
+    // BaseActivity에서 선언한 함수들을 재사용하기위해 오버라이딩
+    // 화면에 데이터를 표시하기 위한 코드 모음
+    override fun setValues() {
+
+    }
+
+
 }
