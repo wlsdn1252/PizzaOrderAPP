f0ceda3fcc06aba03fb4ee492e2fde5be6575257
강진우 <wlsdn1252@naver.com>
2023-01-15 18:44:46 +0900

    탭 레이아웃 제목 및 color설정

--- app/src/main/java/com/example/pizzaorderapp/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp/adapters/MainViewPagerAdapter.kt
@@ -7,6 +7,16 @@ import com.example.pizzaorderapp.fragments.ChickenStoreListFragment
 import com.example.pizzaorderapp.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    // 텝레이아웃의 제목을 적는 함수
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position){
+            0 -> "피자가게"
+            else -> "치킨가게"
+        }
+
+    }
+
     
     // 각각의 position에 맞는 Fragment가 어떤 Fragment인지 명시해주는 함수
     override fun getItem(position: Int): Fragment {
