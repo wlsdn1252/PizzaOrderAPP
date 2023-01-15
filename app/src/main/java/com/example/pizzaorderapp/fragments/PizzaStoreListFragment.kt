e292a2f31466435384961aba9ec14f6eaa1562d9
강진우 <wlsdn1252@naver.com>
2023-01-15 19:37:33 +0900

    실제 데이터 추가 어댑터, 리스트뷰 연결

--- app/src/main/java/com/example/pizzaorderapp/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/example/pizzaorderapp/fragments/PizzaStoreListFragment.kt
@@ -6,8 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pizzaorderapp.R
+import com.example.pizzaorderapp.adapters.PizzaStoreAdapter
+import com.example.pizzaorderapp.datas.Store
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment : Fragment() {
+
+    // ArrayList에 Store를 담아서 멤버 변수로 담아준다.
+    val mPizzaStoreDataList = ArrayList<Store>()
+
+    lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
 
     // fragment_pizza_store_list.xml이랑 연결 시키기
     override fun onCreateView(
@@ -21,5 +29,15 @@ class PizzaStoreListFragment : Fragment() {
     // 동작에 관련된 코드
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        mPizzaStoreDataList.add(Store("A피자가게","1111-5555","https://cdn3.iconfinder.com/data/icons/street-food-and-food-trucker-1/64/pizza-fast-food-bake-bread-128.png"))
+        mPizzaStoreDataList.add(Store("B피자가게","1111-1255","https://cdn1.iconfinder.com/data/icons/cartoon-snack/128/pizza-128.png"))
+        mPizzaStoreDataList.add(Store("C피자가게","1111-5435","https://cdn3.iconfinder.com/data/icons/food-set-3/91/Food_C219-128.png"))
+        mPizzaStoreDataList.add(Store("D피자가게","1111-5565","https://cdn0.iconfinder.com/data/icons/fastfood-31/64/pizza-italian-food-fast-fastfood-cheese-piece-128.png"))
+
+        mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(),R.layout.pizza_store_list_item,mPizzaStoreDataList)
+
+        // 리스트뷰에 실제 화면 연결
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
     }
 }
