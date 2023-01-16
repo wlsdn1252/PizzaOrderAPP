ae8e5e6d23cd3f1420081c8591f2afb89881a0cd
강진우 <wlsdn1252@naver.com>
2023-01-16 19:48:28 +0900

    상세화면에 실제 데이터 반영

--- app/src/main/java/com/example/pizzaorderapp/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp/ViewStoreDetailActivity.kt
@@ -2,9 +2,14 @@ package com.example.pizzaorderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.pizzaorderapp.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
-    // 가게 상세보기 액티비티
+// 가게 상세보기 액티비티
 class ViewStoreDetailActivity : BaseActivity() {
+
+        lateinit var mStoreData : Store
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,12 @@ class ViewStoreDetailActivity : BaseActivity() {
 
         // 화면 관련 함수
     override fun setValues() {
+
+            mStoreData = intent.getSerializableExtra("storeData") as Store
+
+            Glide.with(mContext).load(mStoreData.logoURL).into(storeLogoImg)
+            storePhoneNumTxt.text = mStoreData.phoneNum
+            storeNameTxt.text = mStoreData.name
 
     }
 
